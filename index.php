d0a8ce04d1be0233aa0ddf2aa9a1da57e6a42f49
dimasbasila <dimas.basila@spentera.com>
2023-06-26 13:17:44 +0700

    update repo

--- index.php
+++ index.php
@@ -83,13 +83,15 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
     <?php
-    include 'config_db';
+    include 'config_db.php';
     $data = mysqli_query($conn,"select * from tb_content where content='title_banner'");
+    $row = mysqli_fetch_array($data);
+    $title_banner = $row['value'];
     ?>
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1></h1>
+          <h1><?= $title_banner; ?></h1>
           <h2>We are team of talented designers making websites with Bootstrap</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
--- update.sql
+++ update.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS `tb_content`;
-CREATE TABLE `tb_content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(225) NOT NULL,
-  `date_create` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  DROP TABLE IF EXISTS `tb_content`;
+  CREATE TABLE `tb_content` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `content` varchar(225) NOT NULL,
+    `date_create` datetime NOT NULL,
+    `value` text NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `tb_content` (`id`, `content`, `date_create`) VALUES
-(1,	'content workshop',	'2023-06-03 05:37:32'),
-(2,	'content workshop',	'2023-06-03 05:41:30'),
-(3,	'content workshop',	'2023-06-03 05:41:30');
+  INSERT INTO `tb_content` (`id`, `content`, `date_create`, `value`) VALUES
+  (1,	'title_banner',	'2023-06-03 05:37:32',	'Better Solutions For Your Business');
 
-DROP TABLE IF EXISTS `tb_users`;
-CREATE TABLE `tb_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(225) NOT NULL,
-  `email` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `date_create` datetime NOT NULL,
-  `role` varchar(225) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  DROP TABLE IF EXISTS `tb_users`;
+  CREATE TABLE `tb_users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(225) NOT NULL,
+    `email` varchar(225) NOT NULL,
+    `password` varchar(225) NOT NULL,
+    `date_create` datetime NOT NULL,
+    `role` varchar(225) NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `tb_users` (`id`, `name`, `email`, `password`, `date_create`, `role`) VALUES
-(1,	'admin',	'admin@mail.com',	'$2a$12$6YAJiiBC4kMUSdPWS3.6nOsXaXWlVO7qxhQvEkUhT5D0bmY85pL1u',	'2023-06-10 02:54:47',	'su');
+  INSERT INTO `tb_users` (`id`, `name`, `email`, `password`, `date_create`, `role`) VALUES
+  (1,	'admin1234',	'admin@mail.com',	'$2a$12$6YAJiiBC4kMUSdPWS3.6nOsXaXWlVO7qxhQvEkUhT5D0bmY85pL1u',	'2023-06-10 02:54:47',	'su'),
+  (3,	'John',	'john@mail.com',	'$2y$12$zbq3wpcU/rh5h9VfutiKFONb/VBLE3es4oeYpKyM01t5VbHNRSZq6',	'2023-06-10 05:02:13',	'su');
